03397d85b04e907dddb7dca35736b70242a66576
HenriqueRodriguesRossi <hrodriguescruciakrossi46@gmail.com>
2023-07-26 11:07:27 -0300

    Adicionando conteudo da semana 8

---
+++ Semana 8/busca CEP/github/script.js
@@ -0,0 +1,31 @@
+
+document.getElementById('form-pesquisa').addEventListener('submit', pesquisarPerfil)
+
+
+function pesquisarPerfil(event) {
+  event.preventDefault()
+
+  const username = document.getElementById('username').value
+
+  fetch(`https://api.github.com/users/${username}`) // 
+    .then((response) => {
+      if (response.ok === false) {
+        throw new Error()
+      }
+      return response.json()
+    })
+    .then((data) => {
+      const profileDiv = document.getElementById('profile')
+
+      profileDiv.innerHTML = `
+            <img src=${data.avatar_url} alt="foto do perfil" /> 
+            <p>Nome: ${data.name}</p>
+            <p>Repositórios públicos: ${data.public_repos} </p>
+            <p>Seguidores: ${data.followers} </p>
+          `
+    })
+    .catch(() => {
+      alert("ERRO AO FAZER A SOLICITACAO")
+    })
+
+}
